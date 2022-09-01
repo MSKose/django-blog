ea867ae7f64f7b7e345b994d06392d5ce2142e24
MSKose <saitmustafa1998@gmail.com>
2022-09-01 21:19:37 +0200

    crispy forms added, views updated

--- main/settings.py
+++ main/settings.py
@@ -33,15 +33,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',
+    'users',
+
+
+    "crispy_forms",
+    "crispy_bootstrap5",
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'blog',
-    'users',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +130,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
